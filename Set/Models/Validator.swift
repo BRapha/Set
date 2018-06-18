e986ca71a3c1f92b58b72c7b8e3a7a95aefc01a1
Raphael Blistein <raphael.blistein@gmail.com>
2018-06-18 10:58:16 +0200

    slightly optimized Validator in performance by testing for equality before testing for difference

--- Set/Models/Validator.swift
+++ Set/Models/Validator.swift
@@ -11,33 +11,25 @@ import Foundation
 struct Validator {
     
     /// Checks the validity of a set of cards according to the game's rules.
+    //  Note: Optimized for performance: Falls through to next when 'true' (power of 'OR' statement)
+    //      and returns false as soon as one feature is invalid
     static func isSetValid(_ set: Set<PlayingCard>) -> Bool {
         guard set.count == 3 else { return false }
         
         let colors = set.map { $0.color }
-        let colorValidity = testIfAllDifferent(colors) || testIfAllEqual(colors)
+        let colorValidity = testIfAllEqual(colors) || testIfAllDifferent(colors)
         guard colorValidity else { return false }
         
         let values = set.map { $0.value }
-        let valueValidity = testIfAllDifferent(values) || testIfAllEqual(values)
+        let valueValidity = testIfAllEqual(values) || testIfAllDifferent(values)
         guard valueValidity else { return false }
         
         let shapes = set.map { $0.shape }
-        let shapeValidity = testIfAllDifferent(shapes) || testIfAllEqual(shapes)
+        let shapeValidity = testIfAllEqual(shapes) || testIfAllDifferent(shapes)
         guard shapeValidity else { return false }
         
         let fillings = set.map { $0.filling }
-        return testIfAllDifferent(fillings) || testIfAllEqual(fillings)
-    }
-    
-    private static func testIfAllDifferent<T: Hashable>(_ features: Array<T>) -> Bool {
-        var allDifferent = true
-        for elemet in features.enumerated() {
-            for i in elemet.offset+1 ..< features.endIndex {
-                allDifferent = allDifferent && (elemet.element != features[i])
-            }
-        }
-        return allDifferent
+        return testIfAllEqual(fillings) || testIfAllDifferent(fillings)
     }
     
     private static func testIfAllEqual<T: Hashable>(_ features: Array<T>) -> Bool {
@@ -48,5 +40,15 @@ struct Validator {
             allEqual = allEqual && (first == element.element)
         }
         return allEqual
+    }
+    
+    private static func testIfAllDifferent<T: Hashable>(_ features: Array<T>) -> Bool {
+        var allDifferent = true
+        for elemet in features.enumerated() {
+            for i in elemet.offset+1 ..< features.endIndex {
+                allDifferent = allDifferent && (elemet.element != features[i])
+            }
+        }
+        return allDifferent
     }
 }
